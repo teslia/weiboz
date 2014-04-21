6788c901947d8b18628606b63820d6ff48d24b2a
Zmsky <zmsky@ZmskydeMacBook-Pro.local>
2014-04-21 16:17:39 +0800

    Initial Commit

---
+++ WeiboZ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeiboZ
+//
+//  Created by Zmsky on 14-4-21.
+//  Copyright (c) 2014年 Zmsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeiboZ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeiboZ
+//
+//  Created by Zmsky on 14-4-21.
+//  Copyright (c) 2014年 Zmsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
